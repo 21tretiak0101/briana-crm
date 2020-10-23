ebbee700ff4494ebc7f958c761f75d3aa1c4b8a9
Ilia Tretiak <ilia.tretiak.2001@mail.ru>
2020-10-23 23:59:30 +0300

    feat: add api mapping

---
+++ briana-server/src/main/java/by/ttre16/briana/controller/ApiMapping.java
@@ -0,0 +1,22 @@
+package by.ttre16.briana.controller;
+
+public class ApiMapping {
+    private static final String API = "/api";
+
+    private static final String VERSION = "/v1";
+
+    public static final String EMPLOYEE_REST_URL = API + VERSION + "/employee";
+
+    public static final String CLIENT_REST_URL = API + VERSION + "/client";
+
+    public static final String OWNER_REST_URL = API + VERSION + "/owner";
+
+    public static final String POSITION_REST_URL = API + VERSION + "/position";
+
+    public static final String CATEGORY_REST_URL = API + VERSION + "/category";
+
+    public static final String ORDER_REST_URL = API + VERSION + "/order";
+
+    public static final String PRODUCT_REST_URL = API + VERSION + "/product";
+
+}
