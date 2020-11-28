33860f63ba8c2363cd83b8c806c05faedc11cee1
Ilia Tretiak <ilia.tretiak.2001@mail.ru>
2020-11-28 04:35:54 +0300

    test: update service init method

--- briana-server/src/test/java/by/ttre16/briana/service/AbstractServiceTest.java
+++ briana-server/src/test/java/by/ttre16/briana/service/AbstractServiceTest.java
@@ -2,6 +2,7 @@ package by.ttre16.briana.service;
 
 import by.ttre16.briana.entity.Employee;
 import org.junit.Before;
+import org.springframework.beans.BeanUtils;
 
 import static by.ttre16.briana.data.EmployeeTestData.EMPLOYEE13_ID;
 import static by.ttre16.briana.data.EmployeeTestData.EMPLOYEES;
@@ -13,7 +14,8 @@ public abstract class AbstractServiceTest {
 
     @Before
     public void init() {
-        publisher = EMPLOYEES.get(EMPLOYEE13_ID);
+        publisher = new Employee();
+        BeanUtils.copyProperties(EMPLOYEES.get(EMPLOYEE13_ID), publisher);
         organizationId = publisher.getOrganization().getId();
     }
 }
