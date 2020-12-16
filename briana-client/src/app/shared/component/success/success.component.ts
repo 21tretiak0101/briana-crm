6ed95af5eeb7d761ac00e511228501d727ed851c
Ilia Tretiak <ilia.tretiak.2001@mail.ru>
2020-12-17 00:03:14 +0300

    feat: add success component

---
+++ briana-client/src/app/shared/component/success/success.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-success',
+  styles: ['.success {color: green; font-size: 35px}'],
+  template: '<i class="material-icons success">check_circle_outline</i>'
+})
+export class SuccessComponent { }
