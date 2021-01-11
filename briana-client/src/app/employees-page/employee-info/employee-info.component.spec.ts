5edfda2aaca29af46f2f4c38778454a3ac656d00
Ilia Tretiak <ilia.tretiak.2001@mail.ru>
2021-01-11 23:36:48 +0300

    feat: add employee info component

---
+++ briana-client/src/app/employees-page/employee-info/employee-info.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EmployeeInfoComponent } from './employee-info.component';
+
+describe('EmployeeInfoComponent', () => {
+  let component: EmployeeInfoComponent;
+  let fixture: ComponentFixture<EmployeeInfoComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EmployeeInfoComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EmployeeInfoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
