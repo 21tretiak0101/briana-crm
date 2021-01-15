e34176e973da7b74550ba59052298838dfed2180
Ilia Tretiak <ilia.tretiak.2001@mail.ru>
2021-01-15 23:35:25 +0300

    feat: add employee info component

--- briana-client/src/app/employees-page/employee-info/employee-info.component.ts
+++ briana-client/src/app/employees-page/employee-info/employee-info.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {EmployeeService} from '../../shared/service/employee/employee.service';
+import {ActivatedRoute} from '@angular/router';
+import {switchMap} from 'rxjs/operators';
+import {Employee} from '../../shared/entities';
 
 @Component({
   selector: 'app-employee-info',
@@ -6,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-info.component.css']
 })
 export class EmployeeInfoComponent implements OnInit {
+  employee: Employee;
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService,
+              private activatedRoute: ActivatedRoute) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.activatedRoute.params.pipe(
+      switchMap(params => {
+        return this.employeeService.getById(params.id);
+      })
+    ).subscribe((employee) => {
+      this.employee = employee;
+    });
+  }
 
 }
