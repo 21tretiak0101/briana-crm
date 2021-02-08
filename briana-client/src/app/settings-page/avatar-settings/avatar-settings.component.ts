8bc6161f1f71562eb0e95f0d2b179928f0a33321
Ilia Tretiak <ilia.tretiak.2001@mail.ru>
2021-02-08 22:46:01 +0300

    feat: add generic image handler

--- briana-client/src/app/settings-page/avatar-settings/avatar-settings.component.ts
+++ briana-client/src/app/settings-page/avatar-settings/avatar-settings.component.ts
@@ -1,86 +1,34 @@
-import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
-import {TranslationToken} from '../../shared/service/language/languages';
-import {EmployeeService} from '../../shared/service/employee/employee.service';
-import {MaterialService} from '../../shared/service/material/material.service';
-import {delay} from 'rxjs/operators';
+import {Component, Input} from '@angular/core';
 import {DEFAULT_AVATAR_PATH} from '../../../environments/environment';
+import {ImageService} from '../../shared/service/image/image.service';
+import {EmployeeImageService} from '../../shared/service/image/employee-image/employee-image.service';
+import {AuthService} from '../../shared/service/auth/auth.service';
 
 @Component({
   selector: 'app-avatar-settings',
-  templateUrl: './avatar-settings.component.html',
-  styleUrls: ['./avatar-settings.component.css']
+  template: `
+    <app-generic-image-handler
+      [source]="avatarSrc"
+      [defaultSource]="defaultSource"
+      (successUploadEvent)="onSuccessUpload($event)"
+      (successRemoveEvent)="onSuccessRemove()"
+    ></app-generic-image-handler>
+  `,
+  viewProviders: [
+    {provide: ImageService, useClass: EmployeeImageService}
+  ]
 })
-export class AvatarSettingsComponent implements OnInit {
-  @Input() l: TranslationToken;
-  @ViewChild('inputElement') inputElement: ElementRef;
-  imagePreview: string | ArrayBuffer;
-  loading = false;
-  defaultAvatarPath = DEFAULT_AVATAR_PATH;
+export class AvatarSettingsComponent {
+  @Input() avatarSrc: string;
+  defaultSource = DEFAULT_AVATAR_PATH;
 
-  constructor(private employeeService: EmployeeService) { }
+  constructor(private authService: AuthService) { }
 
-  ngOnInit(): void { }
-
-  onUpload(event: Event): void {
-    console.log(event.target['files'][0]);
-    this.update(event.target['files'][0]);
-    this.resetForm();
+  onSuccessRemove() {
+    this.authService.removeImage();
   }
 
-  trigger() {
-    this.inputElement.nativeElement.click();
-  }
-
-  onRemove() {
-    const decision = window.confirm(this.l.rmQue as string);
-    if (decision) {
-      this.remove();
-    }
-  }
-
-  private update(file: File): void {
-    this.loading = true;
-    this.employeeService.updateImage(file)
-      .pipe(delay(3000))
-      .subscribe(
-      response => {
-        console.log(response);
-        this.setNew(file);
-        this.loading = false;
-      },
-      error => {
-        this.loading = false;
-        MaterialService.toast(error);
-      }
-    );
-  }
-
-  private setNew(file: File): void {
-    const reader = new FileReader();
-    reader.onload = () => {
-      this.imagePreview = reader.result;
-    };
-    reader.readAsDataURL(file);
-  }
-
-  private remove() {
-    this.loading = true;
-    this.employeeService.removeImage()
-      .pipe(delay(2000))
-      .subscribe(
-        response => {
-          console.log(response);
-          this.imagePreview = DEFAULT_AVATAR_PATH;
-          this.loading = false;
-        },
-        error => {
-          MaterialService.toast(error);
-          this.loading = false;
-        }
-      );
-  }
-
-  private resetForm(): void {
-    this.inputElement.nativeElement.value = null;
+  onSuccessUpload(file: File) {
+    this.authService.updateImage(file);
   }
 }
